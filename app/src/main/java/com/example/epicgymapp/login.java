240de7e1752c80d205ebf55a5de0a5c1210ad5fe
iman <20ftt1454@pb.edu.bn>
2022-08-01 22:59:20 +0800

    added signup page

--- app/src/main/java/com/example/epicgymapp/login.java
+++ app/src/main/java/com/example/epicgymapp/login.java
@@ -3,6 +3,9 @@ package com.example.epicgymapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView email = (TextView) findViewById(R.id.email);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button loginbtn = (Button) findViewById(R.id.loginbtn);
+
+        //testing sja
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
