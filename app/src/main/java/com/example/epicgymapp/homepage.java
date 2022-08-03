d5109bf6bac8e8b779ba8a30095f9b7d33a47ff9
iman <20ftt1454@pb.edu.bn>
2022-08-03 12:18:22 +0800

    created imagebutton to redirect to their pages

--- app/src/main/java/com/example/epicgymapp/homepage.java
+++ app/src/main/java/com/example/epicgymapp/homepage.java
@@ -3,6 +3,9 @@ package com.example.epicgymapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class homepage extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        //direct to homepage function
+        ImageButton directHome = (ImageButton)findViewById(R.id.directHome);
+        directHome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText( homepage.this, "Go to Homepage", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //direct to gympage function
+        ImageButton directGym = (ImageButton)findViewById(R.id.directGym);
+        directGym.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText( homepage.this, "Go to Gympage", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //direct to QR page function
+        ImageButton directQR = (ImageButton)findViewById(R.id.directQR);
+        directQR.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText( homepage.this, "Go to QRpage", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //direct to plan page function
+        ImageButton directPlan = (ImageButton)findViewById(R.id.directPlan);
+        directPlan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText( homepage.this, "Go to Plan page", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //direct to shop page function
+        ImageButton directShop = (ImageButton)findViewById(R.id.directShop);
+        directShop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText( homepage.this, "Go to Shop page", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
