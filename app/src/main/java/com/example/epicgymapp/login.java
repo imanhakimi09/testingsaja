182539ebfb35a7183c116968a979c50da624cb61
iman <20ftt1454@pb.edu.bn>
2022-08-06 12:05:24 +0800

    edit login and signup

--- app/src/main/java/com/example/epicgymapp/login.java
+++ app/src/main/java/com/example/epicgymapp/login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,11 +31,26 @@ public class login extends AppCompatActivity {
                     Toast.makeText( login.this, "Login failed", Toast.LENGTH_SHORT).show(); //shows error message
                 }
             });
+
+        //direct to signup button function
+        Button directSignup = (Button)findViewById(R.id.directSignup);
+        directSignup.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                {  openSignup(); }
+            }
+        });
         }
+
         //function to direct to homepage
             public void openHomepage() {
                 Intent intent = new Intent(this, homepage.class);
                 startActivity(intent);
+        }
+
+        //direct to signup
+        public void openSignup(){
+            Intent intent = new Intent(this, signup.class);
+            startActivity(intent);
         }
 
     }
--- app/src/main/java/com/example/epicgymapp/signup.java
+++ app/src/main/java/com/example/epicgymapp/signup.java
@@ -2,6 +2,7 @@ package com.example.epicgymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,15 +16,32 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        EditText username = (EditText) findViewById(R.id.username);
         EditText email = (EditText) findViewById(R.id.email);
+        EditText phone = (EditText) findViewById(R.id.phone);
+        EditText password = (EditText) findViewById(R.id.password);
 
         Button signupbtn = (Button) findViewById(R.id.signupbtn);
+
 
         signupbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText( signup.this, "Account created", Toast.LENGTH_SHORT).show();
+                if(username.getText().toString().equals("iman")
+                        && email.getText().toString().equals("iman.com")
+                        && phone.getText().toString().equals("1234")
+                        && password.getText().toString().equals("iman123")){
+                    {  openHomepage(); }
+                }else{
+                    Toast.makeText( signup.this, "Incomplete form", Toast.LENGTH_SHORT).show(); //shows error message
+                }
             }
         });
+    }
+
+    //function to direct to homepage
+    public void openHomepage() {
+        Intent intent = new Intent(this, homepage.class);
+        startActivity(intent);
     }
 }
