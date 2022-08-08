e57798679b3c3a350196a33f7aab37ac96c2f52c
iman <20ftt1454@pb.edu.bn>
2022-08-08 08:01:25 +0800

    modify database

--- app/src/main/java/com/example/epicgymapp/MainActivity.java
+++ app/src/main/java/com/example/epicgymapp/MainActivity.java
@@ -6,9 +6,6 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class MainActivity extends AppCompatActivity {
     private Button button;
 
