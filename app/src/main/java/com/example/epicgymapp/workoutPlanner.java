0e27d6058db93fe60d12ab2a14af5713974a7176
iman <20ftt1454@pb.edu.bn>
2022-08-28 11:43:30 +0800

    connect workoutplanner to database

--- app/src/main/java/com/example/epicgymapp/workoutPlanner.java
+++ app/src/main/java/com/example/epicgymapp/workoutPlanner.java
@@ -24,9 +24,11 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.HashMap;
 
 public class workoutPlanner extends AppCompatActivity {
-private Button addWorkout;
+private Button addWorkout, increment, decrement;
 private EditText workout;
 private TextView listView;
+private TextView repsValue;
+int count = 5;
 private DatabaseReference reference;
 
 TextInputLayout textInputLayout;
@@ -41,7 +43,9 @@ TextView textView;
         addWorkout = (Button) findViewById(R.id.addWorkoutBtn);
         workout = (EditText) findViewById(R.id.workout);
 //        listView = (TextView) findViewById(R.id.viewOutput);
-
+        repsValue = (TextView) findViewById(R.id.repsValue);
+        increment = (Button) findViewById(R.id.increment);
+        decrement = (Button) findViewById(R.id.decrement);
         textInputLayout = findViewById(R.id.workoutMenu);
         autoCompleteTextView = findViewById(R.id.dropdownItems);
         textView = findViewById(R.id.selectedItem);
@@ -61,12 +65,57 @@ TextView textView;
             @Override
             public void onClick(View view) {
                 String plan = workout.getText().toString().trim();
+                String workout = autoCompleteTextView.getText().toString().trim();
+                String reps = repsValue.getText().toString().trim();
 
                 HashMap<String, String > dataMap = new HashMap<String, String>();
-                dataMap.put("Workout Plan", plan);
+                dataMap.put("Workout Name", plan);
+                dataMap.put("Workout Type", workout);
+                dataMap.put("Reps", reps);
 
                 reference.push().setValue(dataMap);
             }
         });
+        //reps button
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                repsValue.setText("" + count);
+                if(count == 30){
+                    Toast.makeText(workoutPlanner.this, "The maximum reps is 30", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+        //reps button
+        decrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(count <= 5){
+                    Toast.makeText(workoutPlanner.this, "Reps must be more than 5", Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    count--;
+                    repsValue.setText("" + count);
+                }
+            }
+        });
     }
+
+//    public void increment(View view){
+//        count++;
+//        repsValue.setText("" + count);
+//        if(count == 30){
+//            Toast.makeText(this, "The maximum reps is 30", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//    }
+//    public void decrement(View view){
+//        if(count <= 5){
+//            Toast.makeText(this, "Reps must be more than 5", Toast.LENGTH_SHORT).show();
+//            return;
+//        }else count--;
+//        repsValue.setText("" + count);
+//    }
 }
