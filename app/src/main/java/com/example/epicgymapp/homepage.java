d349d6cd9d71115bcb39620a2a90d6f5afdc02a9
iman <20ftt1454@pb.edu.bn>
2022-09-14 20:59:42 +0800

    display welcome user in homepage

--- app/src/main/java/com/example/epicgymapp/homepage.java
+++ app/src/main/java/com/example/epicgymapp/homepage.java
@@ -1,19 +1,60 @@
 package com.example.epicgymapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class homepage extends AppCompatActivity {
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userId;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        //retrieve data
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("New Users");
+        userId = user.getUid();
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                //retrieve and display user profile
+                if(userProfile != null){
+                    String name = userProfile.name;
+
+                    textView2.setText("Hello " + name + ",");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(homepage.this, "Failed to retrieve data!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         //direct to homepage button function
         ImageButton directHome = (ImageButton)findViewById(R.id.directHome);
@@ -51,13 +92,13 @@ public class homepage extends AppCompatActivity {
             }
         });
 
-        //direct to equipments and machine page button function
-        ImageButton directEquip = (ImageButton)findViewById(R.id.machine);
-        directEquip.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                {  openequippage(); }
-            }
-        });
+//        //direct to equipments and machine page button function
+//        ImageButton directEquip = (ImageButton)findViewById(R.id.machine);
+//        directEquip.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                {  openequippage(); }
+//            }
+//        });
 
 
 
@@ -90,8 +131,8 @@ public class homepage extends AppCompatActivity {
     }
 
     //direct to equipments and machines
-    public void openequippage(){
-        Intent intent = new Intent(this, equippage.class);
-        startActivity(intent);
-    }
+//    public void openequippage(){
+//        Intent intent = new Intent(this, equippage.class);
+//        startActivity(intent);
+//    }
 }
