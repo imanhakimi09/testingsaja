9e058b39ba0125c87ad66bc1eb90af2e54fc2348
iman <20ftt1454@pb.edu.bn>
2022-09-17 12:54:56 +0800

    doing diet planner and push data to database

--- app/src/main/java/com/example/epicgymapp/diet_planner.java
+++ app/src/main/java/com/example/epicgymapp/diet_planner.java
@@ -7,31 +7,85 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class diet_planner extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class diet_planner extends AppCompatActivity{
+    //public class diet_planner extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 private Spinner spinner;
+private EditText dietFood;
+TextInputLayout textInputLayout1, textInputLayout2;
+AutoCompleteTextView autoCompleteTextView1, autoCompleteTextView2;
+private Button addDietBtn;
+private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_planner);
-        spinner = (Spinner) findViewById(R.id.spinner1);
-        String[] dietPlan = getResources().getStringArray(R.array.dietType);
+        reference = FirebaseDatabase.getInstance().getReference("DietPlan");
+        dietFood = (EditText) findViewById(R.id.dietFood);
+        textInputLayout1 = (TextInputLayout) findViewById(R.id.dietType);
+        textInputLayout2 = (TextInputLayout) findViewById(R.id.dietMeal);
+        autoCompleteTextView1 = (AutoCompleteTextView) findViewById(R.id.dropdownDiet);
+        autoCompleteTextView2 = (AutoCompleteTextView) findViewById(R.id.dropdownMeal);
+        addDietBtn = (Button) findViewById(R.id.addDietBtn);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, dietPlan);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-    }
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-            if(parent.getId() == R.id.spinner1){
-                String valueFromSpinner = parent.getItemAtPosition(position).toString();
+        String [] diet = {"Carbs", "Protein", "Fat", "Sugar"};
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<>(diet_planner.this, R.layout.item_list, diet);
+        autoCompleteTextView1.setAdapter(itemAdapter);
+        autoCompleteTextView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                textView.setText( parent.getItemAtPosition(position));
             }
-        }
+        });
 
-        @Override
-        public void onNothingSelected(AdapterView<?> parent){
+        String [] meal = {"Breakfast", "Lunch", "Dinner", "Supper"};
+        ArrayAdapter<String> itemAdapter2 = new ArrayAdapter<>(diet_planner.this, R.layout.item_list, meal);
+        autoCompleteTextView2.setAdapter(itemAdapter2);
+        autoCompleteTextView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                textView.setText( parent.getItemAtPosition(position));
+            }
+        });
 
-        }
+        //add plan to database button
+        addDietBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String food = dietFood.getText().toString().trim();
+                String diet = autoCompleteTextView1.getText().toString().trim();
+                String meal = autoCompleteTextView2.getText().toString().trim();
+
+                if(food.isEmpty()){
+                    dietFood.setError("Data cannot be empty");
+                    dietFood.requestFocus();
+                    return;
+                }
+
+                HashMap<String, String > dataMap = new HashMap<String, String>();
+                dataMap.put("Food Name", food);
+                dataMap.put("Diet Plan", diet);
+                dataMap.put("Meal Plan", meal);
+
+                reference.push().setValue(dataMap);
+                Toast.makeText(diet_planner.this, "New Diet plan added", Toast.LENGTH_SHORT).show();
+
+//                getPlan(); //function to retrieve from database
+            }
+        });
+
+    }
+
 }
