dc1b868b1c02c7bc9613556706638bd036c81bd1
iman <20ftt1454@pb.edu.bn>
2022-09-29 23:23:09 +0800

    doing timer for qr page

--- app/src/main/java/com/example/epicgymapp/qrpage.java
+++ app/src/main/java/com/example/epicgymapp/qrpage.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,13 +16,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class qrpage extends AppCompatActivity {
     private Button scanQR, scanOut;
-    private TextView attendance;
-    Timer timer;
+    private TextView attendance, timerTextview;
     private DatabaseReference reference;
 
     @Override
@@ -32,6 +34,7 @@ public class qrpage extends AppCompatActivity {
         scanQR = (Button) findViewById(R.id.scanQR);
         scanOut = (Button) findViewById(R.id.scanOut);
         attendance = (TextView) findViewById(R.id.attendance);
+        timerTextview = (TextView) findViewById(R.id.timerTextview);
 
         //scan qr code function
         scanQR.setOnClickListener(new View.OnClickListener() {
@@ -49,8 +52,29 @@ public class qrpage extends AppCompatActivity {
                         reference.child("Present").setValue("Yes");
                         attendance.setText("Status: Present");
                         Toast.makeText(qrpage.this, "Welcome to EpicGym", Toast.LENGTH_LONG).show();
+
+                        //timer
+                        long duration = TimeUnit.MINUTES.toMillis(1);
+                        new CountDownTimer(duration, 1000) {
+                            @Override
+                            public void onTick(long l) {
+                                String timerCountdown = String.format(Locale.ENGLISH, "%02d : %02d",
+                                        TimeUnit.MILLISECONDS.toMinutes(l),
+                                        TimeUnit.MILLISECONDS.toSeconds(l) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(l)));
+
+                                timerTextview.setText(timerCountdown);
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                reference.child("Present").setValue("No");
+                                attendance.setText("Attendance status");
+                                Toast.makeText(qrpage.this, "Time's Up! Your session is over.", Toast.LENGTH_LONG).show();
+                            }
+                        }.start();
                     }
                 });
+
                 builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -60,17 +84,6 @@ public class qrpage extends AppCompatActivity {
                 });
                 AlertDialog dialog = builder.create();
                 dialog.show();
-
-//                //timer function
-//                timer = new Timer();
-//                timer.schedule(new TimerTask() {
-//                    @Override
-//                    public void run() {
-//                        Toast.makeText(qrpage.this, "Times Up!", Toast.LENGTH_LONG).show();
-//                        attendance.setText("Attendance status");
-//                        finish();
-//                    }
-//                },5000);
             }
         });
 
