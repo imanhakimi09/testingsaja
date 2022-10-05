f080d24e5f53877ec384539fe9991c81ed15b5b6
iman <20ftt1454@pb.edu.bn>
2022-10-05 10:08:12 +0800

    try doing planner in shop and membership page

--- app/src/main/java/com/example/epicgymapp/shoppage.java
+++ app/src/main/java/com/example/epicgymapp/shoppage.java
@@ -1,20 +1,108 @@
 package com.example.epicgymapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class shoppage extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class shoppage extends AppCompatActivity {
+    static ArrayList<String> notes = new ArrayList<String>();
+    static ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.add_note)
+        {
+            Intent intent = new Intent(getApplicationContext(), membershippage.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return false;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoppage);
 
+        ListView listView = (ListView)findViewById(R.id.listView);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    {  openMembership(); }
+            }
+        });
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                Intent intent = new Intent(getApplicationContext(), membershippage.class);
+                intent.putExtra("noteID", position);
+                startActivity(intent);
+            }
+        });
+
+        //double click to edit data
+        //long click to alert user to delete
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id)
+            {
+                new AlertDialog.Builder(shoppage.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Delete?")
+                        .setMessage("Are you sure you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                notes.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+
+                            }
+                        })
+
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
+            }
+        });
+
+        //button to navigate
         //direct to homepage function
         ImageButton directHome = (ImageButton)findViewById(R.id.directHome);
         directHome.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +142,7 @@ public class shoppage extends AppCompatActivity {
     }
     //direct to userprofile
     public void openUserProfile(){
-        Intent intent = new Intent(this, userprofile.class);
+        Intent intent = new Intent(this, membershippage.class);
         startActivity(intent);
     }
     public void openPlanpage(){
@@ -63,6 +151,10 @@ public class shoppage extends AppCompatActivity {
     }
     public void openshoppage(){
         Intent intent = new Intent(this, shoppage.class);
+        startActivity(intent);
+    }
+    public void openMembership(){
+        Intent intent = new Intent(this, membershippage.class);
         startActivity(intent);
     }
 }
