4a28cac5a73e32437202eee7229d22a6c7cc7af4
iman <20ftt1454@pb.edu.bn>
2022-10-21 11:36:58 +0800

    added view workout planner page

---
+++ app/src/main/java/com/example/epicgymapp/viewWorkoutPlanner.java
@@ -0,0 +1,14 @@
+package com.example.epicgymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class viewWorkoutPlanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_workout_planner);
+    }
+}
