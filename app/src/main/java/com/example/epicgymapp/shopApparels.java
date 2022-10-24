50f65fbb066063a63a28f2b307fe504a51b90386
iman <20ftt1454@pb.edu.bn>
2022-10-24 21:00:47 +0800

    back button function in apparels and supllements page

--- app/src/main/java/com/example/epicgymapp/shopApparels.java
+++ app/src/main/java/com/example/epicgymapp/shopApparels.java
@@ -2,13 +2,26 @@ package com.example.epicgymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class shopApparels extends AppCompatActivity {
+    private TextView backTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_apparels);
+        backTextview = (TextView) findViewById(R.id.backTextview);
+
+        backTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(shopApparels.this, shoppage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/epicgymapp/shopSupplements.java
+++ app/src/main/java/com/example/epicgymapp/shopSupplements.java
@@ -2,13 +2,27 @@ package com.example.epicgymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class shopSupplements extends AppCompatActivity {
+    private TextView backTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_supplements);
+
+        backTextview = (TextView) findViewById(R.id.backTextview);
+
+        backTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(shopSupplements.this, shoppage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
