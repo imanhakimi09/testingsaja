da0cbac96985045c80285f3e186f5db23bc64ca3
iman <20ftt1454@pb.edu.bn>
2022-10-25 14:04:39 +0800

    design shirt in apparels page

--- app/src/main/java/com/example/epicgymapp/shopApparels.java
+++ app/src/main/java/com/example/epicgymapp/shopApparels.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class shopApparels extends AppCompatActivity {
     private TextView backTextview;
+
+    private Button  shirt1Button, shirt2Button, shirt3Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,36 @@ public class shopApparels extends AppCompatActivity {
         setContentView(R.layout.activity_shop_apparels);
         backTextview = (TextView) findViewById(R.id.backTextview);
 
+        shirt1Button = (Button) findViewById(R.id.shirt1Button);
+        shirt2Button = (Button) findViewById(R.id.shirt2Button);
+        shirt3Button = (Button) findViewById(R.id.shirt3Button);
+
+
         backTextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(shopApparels.this, shoppage.class);
+                startActivity(intent);
+            }
+        });
+
+        shirt1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(shopApparels.this, shoppage.class);
+                startActivity(intent);
+            }
+        });
+
+        shirt2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(shopApparels.this, shoppage.class);
+                startActivity(intent);
+            }
+        });
+
+        shirt3Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(shopApparels.this, shoppage.class);
